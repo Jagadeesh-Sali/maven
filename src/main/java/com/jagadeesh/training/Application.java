e84d527494ada4070a88aef739cb9132495c333f
Jagadeesh Sali <jagadeesh.gs@gmail.com>
2018-06-13 14:20:48 +0530

    Modifying pom.xml to add Apache Commons Lang3 dependency

--- src/main/java/com/jagadeesh/training/Application.java
+++ src/main/java/com/jagadeesh/training/Application.java
@@ -2,8 +2,14 @@ package com.jagadeesh.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	
+	public int countWords(String text){
+		String[] words = StringUtils.split(text, " ");
+		return (words == null)? 0: words.length;
+	}
 
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -24,5 +30,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int wordsCount = app.countWords("I Have Four Words");
+		System.out.println("Total Words: "+wordsCount);
     }
 }
